95c1386b037e5b80dd9aac65dbbd314fae68ded1
Alex <fakeacc666@bk.ru>
2020-02-25 02:49:09 +0300

    add basic api service and http utils

--- public/src/apiService.js
+++ public/src/apiService.js
@@ -1,41 +1,53 @@
 'use strict';
 
+import {fetchGetJson, fetchPostJson} from './httpUtils.js'
+
 export default class ApiService {
     constructor(address) {
         this.address = address;
     }
 
     /**
-     *
+     * @description Register user
      * @param userInfo - user info object (name, surname, nickname, password)
      * @returns {Promise<Response>}
      */
     join(userInfo) {
         const apiUrl = new URL('join', this.address);
-
-        return fetch(apiUrl.href, {
-            method: 'POST',
-            mode: "cors",
-            credentials: 'include',
-            body: JSON.stringify(userInfo),
-        })
+        return fetchPostJson(apiUrl, userInfo);
     }
 
     /**
-     *
+     * @description Login user; create user session
      * @param userInfo - user info object (nickname, password)
      * @returns {Promise<Response>}
      */
     login(userInfo) {
         const apiUrl = new URL('login', this.address);
-
-        return fetch(apiUrl.href, {
-            method: 'POST',
-            mode: "cors",
-            credentials: 'include',
-            body: JSON.stringify(userInfo)
-        })
+        return fetchPostJson(apiUrl, userInfo);
     }
 
+    // TODO(Alexandr): logout
+
+    // TODO(Alexandr): postUser jsdoc
+    /**
+     * @description Update profile info
+     * @param userInfo -
+     * @returns {Promise<Response>}
+     */
+    postUser(userInfo) {
+        const apiUrl = new URL('profile', this.address);
+        return fetchPostJson(apiUrl, userInfo)
+    }
+
+    /**
+     * @description Get profile info
+     * @param userInfo - user info object (nickname)
+     * @returns {Promise<Response>}
+     */
+    getUser(userInfo) {
+        const apiUrl = new URL('profile', this.address);
+        return fetchGetJson(apiUrl, userInfo)
+    }
 
 }
