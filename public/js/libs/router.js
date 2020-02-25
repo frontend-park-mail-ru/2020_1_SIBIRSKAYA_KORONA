2a7fc6aa8df4eff59e1c51cf3a5110d902fdd2b1
gavroman <gavroman1998@gmail.com>
2020-02-25 15:07:19 +0300

    JSDoc added

--- public/js/libs/router.js
+++ public/js/libs/router.js
@@ -1,6 +1,13 @@
 'use strict';
 
-class Router {
+/**
+ * Application router
+ */
+export default class Router {
+    /**
+     * Router constructor
+     * @param {object} root - application's root element
+     */
     constructor(root) {
         this.root = root;
         this.routeMap = new Map();
@@ -8,12 +15,20 @@ class Router {
         this.root.addEventListener('click', this.handleMouseClick);
     }
 
+    /**
+     * Switch current route
+     * @param {string} route - route to go
+     */
     go(route) {
         if (this.routeMap.has(route)) {
             this.routeMap.get(route)();
         }
     }
 
+    /**
+     * Click handler
+     * @param {object} e - mouse event
+     */
     handleMouseClick(e) {
         if (e.target.tagName.toLowerCase() === 'a') {
             e.stopPropagation();
@@ -21,13 +36,15 @@ class Router {
             this.go(e.target.pathname);
         } else if (e.target.tagName.toLowerCase() === 'button') {
             e.preventDefault();
-
         }
     }
 
+    /**
+     * Set route and function to call for it
+     * @param {string} route - route to go
+     * @param {function} handler - route handler
+     */
     setRoute(route, handler) {
         this.routeMap.set(route, handler);
     }
 }
-
-export default Router;
