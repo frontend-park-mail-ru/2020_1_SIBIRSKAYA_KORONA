9e7c0309d64909b475372d2798a05d1c3bf1ce38
gavroman <gavroman1998@gmail.com>
2020-02-28 16:12:07 +0300

    Deploy test

--- public/js/controllers/joinControl.js
+++ public/js/controllers/joinControl.js
@@ -2,7 +2,14 @@ import JoinModel from '../models/joinModel.js';
 import JoinView from '../views/join/joinView.js';
 import EventBus from '../libs/eventBus.js';
 
+/**
+ * Join(Register) controller
+ */
 export default class JoinController {
+    /**
+     * Controller constructor
+     * @param {Object} router - for model to redirect on success registration
+     */
     constructor(router) {
         this.eventBus = new EventBus([
             'submit',
