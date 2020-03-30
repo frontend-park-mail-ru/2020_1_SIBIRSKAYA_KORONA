42a121a9dec26d37fe265b651b025cf167309510
gavroman <gavroman1998@gmail.com>
2020-03-30 22:09:09 +0300

    No copypaste in boardView.setHandlers()

--- public/js/views/board/boardView.js
+++ public/js/views/board/boardView.js
@@ -53,49 +53,6 @@ export default class BoardView extends BaseView {
         buttons.forEach((button) => {
             button.addEventListener('click', this.handleButtonClick.bind(this));
         });
-
-        /*        // ADD NEW MEMBER
-                const addNewMemberButton = document.getElementById('inviteNewMemberButton');
-                addNewMemberButton.addEventListener('click', () => {
-                    this.eventBus.call('inviteNewMember');
-                });
-
-                // ADD NEW CARD
-                const addNewColumnButton = document.getElementById('addNewColumnButton');
-                addNewColumnButton.addEventListener('click', () => {
-                    this.eventBus.call('addNewColumn');
-                });
-
-                // ADD NEW TASK
-                const addNewTaskButtons = Array.from(document.getElementsByName('addNewTaskButton'));
-                addNewTaskButtons.forEach((button) => {
-                    button.addEventListener('click', () => {
-                        this.eventBus.call('addNewTask');
-                    });
-                });
-
-                // BOARD SETTINGS
-                const boardSettingsButton = document.getElementById('boardSettingsButton');
-                boardSettingsButton.addEventListener('click', () => {
-                    this.eventBus.call('openBoardSettings');
-                });
-
-                // CARD SETTINGS
-                const columnSettingsButtons = Array.from(document.getElementsByName('columnSettingsButton'));
-                columnSettingsButtons.forEach((button) => {
-                    button.addEventListener('click', () => {
-                        this.eventBus.call('openColumnSettings');
-                    });
-                });
-
-                // TASK SETTINGS
-                const taskSettingsButtons = Array.from(document.getElementsByName('taskSettingsButton'));
-                taskSettingsButtons.forEach((button) => {
-                    button.addEventListener('click', (event) => {
-                        event.stopPropagation();
-                        this.eventBus.call('openTaskSettings', event.currentTarget);
-                    });
-                });*/
     }
 
     /**
