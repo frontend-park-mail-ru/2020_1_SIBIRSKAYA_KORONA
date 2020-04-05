035ac6b6fbddcf65addbe63a00a09922b3d3b53f
gavroman <gavroman1998@gmail.com>
2020-04-06 02:35:16 +0300

    npm prestart

--- run_frontend.sh
+++ run_frontend.sh
@@ -6,6 +6,8 @@ echo "Run frontend"
 cd $FRONTEND_DIR
 git checkout deploy && git pull
 npm install
+
+npm run prestart
 ./node_modules/.bin/forever stopall
 ./node_modules/.bin/forever start ./server/server.js
 echo "Forntend is running"
