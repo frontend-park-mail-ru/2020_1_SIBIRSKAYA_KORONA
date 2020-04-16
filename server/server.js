c3b0ac2a0f1c4a7de95fb39355751cb119969563
Lebedev Alexandr <s.lebedev.2011@bk.ru>
2020-04-16 13:39:23 +0300

    add redirect to https

--- server/server.js
+++ server/server.js
@@ -6,26 +6,35 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 
-
-const app = express();
+// INIT HTTPS SERVER
 const publicFolder = path.resolve(__dirname, '..', 'public');
 const distFolder = path.resolve(publicFolder, 'dist');
 
-app.use(morgan('dev'));
-app.use(express.static(distFolder));
-app.use(express.static(publicFolder));
+const appHttps = express();
+appHttps.use(morgan('dev'));
+appHttps.use(express.static(distFolder));
+appHttps.use(express.static(publicFolder));
 
 
-app.get('*', (req, res) => {
+appHttps.get('*', (req, res) => {
     res.sendFile(path.resolve(publicFolder, 'index.html'));
 });
 
-const httpServer = http.createServer(app);
-httpServer.listen(80, () => console.log(`HTTP server started`));
 
 const httpsServer = https.createServer({
     key: fs.readFileSync('server/credentials/prod.key'),
     cert: fs.readFileSync('server/credentials/prod.crt'),
-}, app);
+}, appHttps);
 httpsServer.listen(443, () => console.log(`HTTPS server started`));
 
+
+// INIT HTTP REDIRECT SERVER
+const appHttp = express();
+appHttp.use(morgan('dev'));
+
+appHttp.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+});
+
+const httpServer = http.createServer(appHttp);
+httpServer.listen(80, () => console.log(`HTTP server started`));
